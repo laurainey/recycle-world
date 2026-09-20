86ebf64f6621059332971d215a960077136a5101
laurainey <lauraineyehouea@gmail.com>
2026-09-20 13:11:56 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -1,16 +1,17 @@
 import pgzrun
-import random
+import random 
 
 WIDTH = 800
 HEIGHT = 600
 CENTRE_X = 400
 CENTRE_Y = 300
-TITLE = "Healty food game"
-FINAL_LEVEL = 8
-FINAL_SPEED = 10
-ITEMS = ["cake","french_fries","pizza","pancake"]
+CENTRE = (CENTRE_X,CENTRE_Y)
+TITLE = "Recycle world"
+FINAL_LEVEL = 6
+START_SPEED = 10
+ITEMS = ["bottle","battery","plastic","chips"]
 
-game_over = False
+game_over = False 
 game_complete = False
 current_level = 1
 
@@ -18,32 +19,51 @@ items = []
 animations = []
 
 def draw():
+    global items, game_complete, current_level, game_over
     screen.clear()
-    screen.blit("bg_foodimg", (0,0))
-
+    screen.blit("bg", (0,0))
+    if game_over:
+        display_message("GAME OVER", "Try again")
+    elif game_complete:
+        display_message("YOU WON","Great job, recycler")
+    else:
+        for item in items:
+            item.draw()
+    
 def update():
-    pass
+    global items 
+    if  len(items) == 0 :
+        items = make_items(current_level)
 
-def get_option (extra_item):
-    items_to_create = ["salad"]
-    for i in range(extra_item):
+
+def make_items(extra_items):
+    items_to_create = get_option(extra_items)
+    new_items = create_items(items_to_create)
+    layout_items(new_items)
+    animate_items(new_items)
+    return new_items
+
+def get_option(extra_items):
+    items_to_create = ["paper"]
+    for i in range (extra_items):
         random_option = random.choice(ITEMS)
         items_to_create.append(random_option)
-    return items_to_create 
+    return items_to_create
 
 def create_items(items_to_create):
     new_items = []
     for option in items_to_create:
-        item = Actor(option + "img")
-        new_items.append(item)
+       item = Actor(option + "img")  
+       new_items.append(item)
     return new_items
+
 def layout_items(items_to_layout):
-    number_of_gaps = len(items_to_layout)
+    number_of_gaps = len(items_to_layout) +1
     gap_size = WIDTH / number_of_gaps
-    random.shuffle(items_to_layout)
+    random.shuffle(items_to_layout) 
     for index,item in enumerate(items_to_layout):
-        new_y_pos = (index + 1) * gap_size 
-        item.x = new_y_pos
+        new_x_pos = (index +1)* gap_size
+        item.x = new_x_pos
 
 def animate_items(items_to_animate):
     global animations
@@ -51,7 +71,7 @@ def animate_items(items_to_animate):
         duration = START_SPEED - current_level
         item.anchor = ("center","bottom")
         animation = animate(item,duration = duration, on_finished = handle_gameover, y = HEIGHT)
-        animations.append(item)
+        animations.append(animation)
 def handle_gameover():
     global game_over 
     game_over = True
@@ -79,5 +99,10 @@ def stop_animations(animations_to_stop):
     for animation in animations_to_stop:
         if animation.running:
            animation.stop()
-        
+
+def display_message(main_text,sub_text):
+    screen.draw.text(main_text,fontsize= 60 , center = CENTRE, color = "blue")
+    screen.draw.text(sub_text,fontsize = 40 ,center = (CENTRE_X,CENTRE_Y + 30),color = "purple" )
+
+
 pgzrun.go()
