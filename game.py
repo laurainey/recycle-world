0a28ac8eae2f7fd6ed9b8985234cc72d9df95fb5
laurainey <lauraineyehouea@gmail.com>
2026-09-20 13:11:19 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -1,17 +1,16 @@
 import pgzrun
-import random 
+import random
 
 WIDTH = 800
 HEIGHT = 600
 CENTRE_X = 400
 CENTRE_Y = 300
-CENTRE = (CENTRE_X,CENTRE_Y)
-TITLE = "Recycle world"
-FINAL_LEVEL = 6
-START_SPEED = 10
-ITEMS = ["bottle","battery","plastic","chips"]
+TITLE = "Healty food game"
+FINAL_LEVEL = 8
+FINAL_SPEED = 10
+ITEMS = ["cake","french_fries","pizza","pancake"]
 
-game_over = False 
+game_over = False
 game_complete = False
 current_level = 1
 
@@ -20,32 +19,31 @@ animations = []
 
 def draw():
     screen.clear()
-    screen.blit("bg", (0,0))
+    screen.blit("bg_foodimg", (0,0))
 
 def update():
     pass
 
-def get_option(extra_items):
-    items_to_create = ["paper"]
-    for i in range (extra_items):
+def get_option (extra_item):
+    items_to_create = ["salad"]
+    for i in range(extra_item):
         random_option = random.choice(ITEMS)
         items_to_create.append(random_option)
-    return items_to_create
+    return items_to_create 
 
 def create_items(items_to_create):
     new_items = []
     for option in items_to_create:
-       item = Actor(option + "img")  
-       new_items.append(item)
+        item = Actor(option + "img")
+        new_items.append(item)
     return new_items
-
 def layout_items(items_to_layout):
-    number_of_gaps = len(items_to_layout) +1
+    number_of_gaps = len(items_to_layout)
     gap_size = WIDTH / number_of_gaps
-    random.shuffle(items_to_layout) 
+    random.shuffle(items_to_layout)
     for index,item in enumerate(items_to_layout):
-        new_x_pos = (index +1)* gap_size
-        item.x = new_x_pos
+        new_y_pos = (index + 1) * gap_size 
+        item.x = new_y_pos
 
 def animate_items(items_to_animate):
     global animations
@@ -81,15 +79,5 @@ def stop_animations(animations_to_stop):
     for animation in animations_to_stop:
         if animation.running:
            animation.stop()
-
-
-
-
-    
-    
-
-
-
+        
 pgzrun.go()
-
-
